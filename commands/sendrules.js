38bea7bd513c65e8d5a1fa2b3d91150f06c286f3
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2022-05-23 19:31:53 +0800

    More changes and stuff

--- commands/sendrules.js
+++ commands/sendrules.js
@@ -1,5 +1,6 @@
 const rules = require("../rules/rules.json")
 const staff_rules = require("../rules/staffrules.json")
+const consequences = require("../rules/dontfollow.json")
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
@@ -17,7 +18,7 @@ module.exports = {
             await interaction.reply({ content: "That isn't a text channel smfh.", ephemeral: true })
             return 
         }
-        await channel.send({ embeds: [rules, staff_rules] })
+        await channel.send({ embeds: [rules, staff_rules, consequences] })
         await interaction.reply(`Successfully sent rules to ${channel}`)
     }
 }
