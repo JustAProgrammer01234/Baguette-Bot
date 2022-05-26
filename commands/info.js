f02529042e1a130cc12d944b820cc7d67f79c30b
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2022-05-26 08:01:44 +0800

    Added functionality to guild option.

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
+const { SlashCommandBuilder, time } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription("Gets info from a member, this server, or the bot itself.")
         .addStringOption(option =>
             option.setName("option")
-                .setDescription("choose shit")
+                .setDescription("choose an option")
                 .setRequired(true)
                 .addChoices(
                     {name: "This server", value: "guild"},
@@ -19,11 +19,30 @@ module.exports = {
         const option = interaction.options.getString("option")
         const embed = new MessageEmbed()
 
+        embed.setColor("#FBCEB1")
+
         switch (option) {
             case "guild":
-                embed.setTitle("Information about this cool server:")
-                embed.setDescription(interaction.guild.description)
-                embed.setThumbnail(interaction.guild.iconURL())
+                const guild = interaction.guild 
+
+                await guild.members.fetch()
+                const owner = await guild.fetchOwner()
+
+                const users = guild.members.cache.filter(m => !m.user.bot).size 
+                const bots = guild.members.cache.filter(m => m.user.bot).size 
+                const text_channels = guild.channels.cache.filter(c => c.isText()).size
+                const voice_channels = guild.channels.cache.filter(c => c.isVoice()).size
+                const category_channels = guild.channels.cache.filter(c => c.type == "GUILD_CATEGORY").size
+
+                embed.setTitle("Information about this uhhh server:")
+                embed.setDescription(guild.description)
+                embed.setThumbnail(guild.iconURL())
+                embed.addFields(
+                    {name: "Created at:", value: time(guild.createdAt, "F")},
+                    {name: "Owner:", value: owner.toString()},
+                    {name: "Members:", value: `Users: ${users}\nBots: ${bots}\nTotal: ${guild.memberCount}`, inline: true}, 
+                    {name: "Channels:", value: `Text Channels: ${text_channels}\nVoice Channels: ${voice_channels}\nCategory Channels: ${category_channels}\nTotal: ${guild.channels.channelCountWithoutThreads}`, inline: true}
+                )
                 break 
             case "member":
                 embed.setTitle("a")
